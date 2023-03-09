5feca30505be5f7221a3a416bfae366193a89d47
stuWolf <13439@coderacademy.edu.au>
2023-03-09 12:49:30 +1100

    add models and schemas

--- src/model/models.py
+++ src/model/models.py
@@ -9,3 +9,49 @@ class User(db.Model):
     verified = db.Column(db.Boolean())
     mobile_number = db.Column(db.String())
     post_code = db.Column(db.String())
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer(), primary_key=True)
+
+    progres = db.Column(db.String(), nullable=False, unique=True)
+    date = db.Column(db.Boolean())
+    user_id = db.Column(
+        db.Integer(), db.ForeignKey("users.id"), nullable=False
+    )
+
+    user = db.relationship('User', backref='workouts')
+
+class Workout_Exercise(db.Model):
+    __tablename__ = "workout_exercises"
+
+    id = db.Column(db.Integer(), primary_key=True)
+
+    progres = db.Column(db.String(), nullable=False, unique=True)
+    date = db.Column(db.Boolean())
+    workout_id = db.Column(
+        db.Integer(), db.ForeignKey("workouts.id"), nullable=False
+    )
+    exercise_id = db.Column(
+        db.Integer(), db.ForeignKey("exercises.id"), nullable=False
+    )
+
+    workout = db.relationship('Workout', backref='workout_exercises')
+    exercise = db.relationship('Exercise', backref='workout_exercises')
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer(), primary_key=True)
+
+    name = db.Column(db.String(), nullable=False, unique=True)
+    description = db.Column(db.String())
+    interval_time = db.Column(db.String())
+    repetitions = db.Column(db.String())
+    rest_time = db.Column(db.String())
+    rounds = db.Column(db.String())
+    muscle_group = db.Column(db.String())
+    level = db.Column(db.String())
+    
--- src/schema/schemas.py
+++ src/schema/schemas.py
@@ -2,9 +2,42 @@ from main import ma
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "username", "verified", "mobile_number", "post_code", "pets")
+        fields = ("id", "username", "verified", "mobile_number", "post_code")
 
-    pets = ma.List(ma.Nested("PetSchema", exclude=("user",)))
+    # pets = ma.List(ma.Nested("PetSchema", exclude=("user",)))
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
+
+class WorkoutSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "progres", "date")
+        load_only = ["user_id"]
+
+    user = ma.Nested("UserSchema")
+
+    # pets = ma.List(ma.Nested("PetSchema", exclude=("user",)))
+
+workout_schema = WorkoutSchema()
+workouts_schema = WorkoutSchema(many=True)
+
+
+class Workout_ExerciseSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "date")
+        load_only = ["workout_id, exercise_id"]
+
+    workout = ma.Nested("WorkoutSchema")
+    exercise = ma.Nested("ExerciseSchema")
+workout_exercise_schema = Workout_ExerciseSchema()
+workout_exercises_schema = Workout_ExerciseSchema(many=True)
+
+class ExerciseSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "name", "description", "interval_time", "repetitions", "rest_time", "rounds", "muscle_group", "level")
+        
+
+   
+
+user_schema = ExerciseSchema()
+users_schema = ExerciseSchema(many=True)
